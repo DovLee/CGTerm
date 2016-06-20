464265f54b1dfe328f279ba2014fc429baa8ab8c
DovLee <ann724@naver.com>
2016-06-20 18:28:45 +0900

    Update frag_lighting_view.cpp

--- tizen_gles_sample/src/sample/frag_lighting_view.cpp
+++ tizen_gles_sample/src/sample/frag_lighting_view.cpp
@@ -6,18 +6,36 @@ using namespace std;
 
 void FragLightingView::OnInit()
 {
+	/*
 	string vs = FileLoader::GetInstance()->ReadTxtFile("shader/view_f_lit/f_lit.vs");
 	string fs = FileLoader::GetInstance()->ReadTxtFile("shader/view_f_lit/f_lit.fs");
+	mViewRenderer->SetProgram(vs, fs);
+	*/
+	string vs = FileLoader::GetInstance()->ReadTxtFile("shader/view_nor/nor.vs");
+	string fs = FileLoader::GetInstance()->ReadTxtFile("shader/view_nor/nor.fs");
 	mViewRenderer->SetProgram(vs, fs);
 
 	string ball = FileLoader::GetInstance()->ReadTxtFile("obj3d/sphere3");
 
+	/*
 	TexData textJ;
 	FileLoader::GetInstance()->ReadTexture("tex/soccer.jpg", textJ);
+	*/
+	TexData tex, normal_tex;
+	//height_tex;
+	FileLoader::GetInstance()->ReadTexture("tex/photosculpt-graystonewall-diffuse.jpg", tex);
+	FileLoader::GetInstance()->ReadTexture("tex/photosculpt-graystonewall-normal.jpg", normal_tex);
+	//FileLoader::GetInstance()->ReadTexture("tex/photosculpt-graystonewall-ambientocclusion.jpg", height_tex);
 
+	/*
 	float scale = 30.0f;
 	mViewRenderer->SetNewModel(ball, scale);
 	mViewRenderer->SetTexture(TEXDATA_GENERAL, &textJ);
+	*/
+	float scale = 30.0f;
+	mViewRenderer->SetNewModel(ball, scale);
+	mViewRenderer->SetTexture(TEXDATA_GENERAL, &tex);
+	mViewRenderer->SetTexture(TEXDATA_NORMAL_MAP, &normal_tex);
 
 	mViewRenderer->Initialize();
 	glClearColor(0.1f, 0.1f, 0.3f, 0.0f);
